454772cb5a77c988653ea0bef64242ef23fcd3ba
Alexander Pinkerton <alexpinkerton88@gmail.com>
2016-06-07 19:32:56 -0400

    Increased speed and clarity of parallax

--- components/parallax.ts
+++ components/parallax.ts
@@ -11,24 +11,49 @@ import {Component} from 'angular2/core';
 
 
 export class Parallax {
+  
   constructor() {
+    this.lastX = 0;
+    this.lastY = 0;
+    this.offsetY = -2500;
+    this.offsetX = -800;
     
+    //Parallax Distances
+    this.minDistance = 25;
+    this.distanceLayer1 = 20;
+    this.distanceLayer2 = 10;
+    this.distanceLayer3 = 5;
+    this.distanceLayer4 = 2;
   }
 
-  moveLayers(event) {
-    
-    var OffsetY = -2500;
-    var OffsetX = -800;
-    var x = -(event.pageX + OffsetY);
-    var y = -(event.pageY + OffsetX);
+  
+  
 
-    $('.layer1').css('background-position', x / 20 + 'px ' + y / 20 + 'px');   
-    $('.layer2').css('background-position', x / 10 + 'px ' + y / 10 + 'px');   
-    $('.layer3').css('background-position', x / 5 + 'px ' + y / 5 + 'px');  
-    $('.layer4').css('background-position', x / 2 + 'px ' + y / 2 + 'px');
+
+  moveLayers(event) {
+
+    //Move background if mouse has moved minimum distance to save computation
+    if (Math.abs(event.pageX - this.lastX) > this.minDistance || Math.abs(event.pageY - this.lastY) > this.minDistance) {
+      
+      //Apply Offset
+      var x = -(event.pageX + this.offsetY);
+      var y = -(event.pageY + this.offsetX);
+
+      //Move the background
+      document.getElementsByClassName('layer1')[0].style.backgroundPosition = x / this.distanceLayer1 + 'px ' + y / this.distanceLayer1 + 'px';
+      document.getElementsByClassName('layer2')[0].style.backgroundPosition = x / this.distanceLayer2 + 'px ' + y / this.distanceLayer2 + 'px';
+      document.getElementsByClassName('layer3')[0].style.backgroundPosition = x / this.distanceLayer3 + 'px ' + y / this.distanceLayer3 + 'px';
+      document.getElementsByClassName('layer4')[0].style.backgroundPosition = x / this.distanceLayer4 + 'px ' + y / this.distanceLayer4 + 'px';
+
+      //Update the last X and Y where a shift occurred.
+      this.lastX = event.pageX;
+      this.lastY = event.pageY;
+    }
+
+
+
+    
+
   }
 
 }
-
-
-//bootstrap(Parallax);
